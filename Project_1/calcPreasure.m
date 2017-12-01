99d6468fb085ccb2b71ce153afe1ec71a46be9ce
WIN-E33SFQICM85\User <yuriimytiai@gmail.com>
2017-12-01 14:13:17 +0200

    added air absorbtion

--- Project_1/calcPreasure.m
+++ Project_1/calcPreasure.m
@@ -27,8 +27,11 @@ theta = thetaRS + round(source.theta0); % include rotation in two dimensionals
 phi = phiRS + round(source.phi0);
 tau = source.tau0;
 
-A = source.SSobj.sensitivity(freq_idx) + source.K - 20*log10(r) + source.SSobj.amplitudeRP(phi, theta); % amplitude value of preasure in dB
+alpha = 1.24e-11*source.SSobj.f(freq_idx)^2; % air absorbtion coef.
+
+A = source.SSobj.sensitivity(freq_idx) + source.K - 20*log10(r) + source.SSobj.amplitudeRP(phi, theta) - 20*alpha*r*log10(exp(1)); % amplitude value of preasure in dB
 Psi = 2*pi*source.SSobj.f(freq_idx) * (r/c0 + tau) + source.SSobj.phaseRP(phi, theta); % phase of preasure in rad
+
 
 end
 
